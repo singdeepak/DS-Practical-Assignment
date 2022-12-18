02369bb370a269ba4fdf1b6b24b3ae2ead349e2e
Deepak Singh <immrdeepaksingh@gmail.com>
2022-12-18 21:46:23 +0530

    v3

---
+++ Ans-16.c
@@ -0,0 +1,269 @@
+/*WAP to demonstrate following operations on singly linked list:
+Insert a node
+
+I. at the beginning of list
+II. at given position of list
+III. at the end of list
+
+delete a node
+
+I. from the beginning of list
+II. from given position of list
+III. from the end of list
+
+Traversal of the list*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *head = NULL;
+
+struct node *create_node()
+{
+    struct node *temp;
+    temp = (struct node *)malloc(sizeof(struct node));
+    return temp;
+}
+
+void at_begin()
+{
+    struct node *ptr;
+    int d;
+    ptr = create_node();
+
+    printf("\nEnter data value : ");
+    scanf("%d", &d);
+    ptr->data = d;
+
+    if (head == NULL)
+    {
+        ptr->next = NULL;
+        head = ptr;
+        printf("\nNode was inserted at beginning\n\n");
+    }
+
+    else
+    {
+        ptr->next = head;
+        head = ptr;
+        printf("\nNode was inserted at beginning\n\n");
+    }
+}
+
+void at_random()
+{
+    struct node *ptr, *trav;
+    int loc, d, k;
+    ptr = create_node();
+
+    printf("\nEnter node location : ");
+    scanf("%d", &loc);
+    printf("\nEnter data value : ");
+    scanf("%d", &d);
+    ptr->data = d;
+
+    if (loc == 1)
+    {
+        ptr->next = head;
+        head = ptr;
+        printf("\nNode has been inserted\n\n");
+    }
+    else
+    {
+        trav = head;
+        for (k = 1; k < loc - 1; k++)
+        {
+            trav = trav->next;
+            if (trav == NULL)
+            {
+                printf("\nCan't insert node, list got overflow\n\n");
+                return;
+            }
+        }
+        ptr->next = trav->next;
+        trav->next = ptr;
+        printf("\nNode has been inserted\n\n");
+    }
+}
+
+void at_last()
+{
+    struct node *ptr, *trav;
+    int d;
+    ptr = create_node();
+
+    printf("\nEnter data value : ");
+    scanf("%d", &d);
+    ptr->data = d;
+
+    if (head == NULL)
+        printf("\nCan't insert, list is empty\n\n");
+    else
+    {
+        trav = head;
+        while (trav->next != NULL)
+            trav = trav->next;
+        trav->next = ptr;
+        ptr->next = NULL;
+        printf("\nNode was inserted at last\n\n");
+    }
+}
+
+void delete_first()
+{
+    struct node *temp;
+    if (head == NULL)
+        printf("\nList is empty\n\n");
+    else
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
+        printf("\nFirst node was deleted\n\n");
+    }
+}
+
+
+
+
+
+void delete_last()
+{
+    struct node *temp, *ptr;
+    if (head == NULL)
+        printf("\nList is empty\n\n");
+    else if(head->next == NULL)
+    {
+        head = NULL;
+        free(head);
+        printf("\nThe only was deleted\n\n");
+    }
+    else{
+        ptr = head;
+        while(ptr->next!=NULL)
+        {
+            temp = ptr;
+            ptr = ptr->next;
+        }
+        temp->next = NULL;
+        free(ptr);
+        printf("\nLast node was deleted\n\n");
+    }
+}
+
+
+
+void delete_random()
+{
+    struct node *temp, *ptr;
+    int loc, k;
+
+    if(head == NULL)
+        printf("\nList is empty\n\n");
+    else{
+        printf("\nEnter node location that you want to delete : ");
+        scanf("%d",&loc);
+
+        ptr = head;
+        for(k=0; k<loc-1; k++)
+        {
+            temp = ptr;
+            ptr = ptr->next;
+        }
+        temp->next = ptr->next;
+        free(ptr);
+        printf("\nNode was deleted\n\n");
+    }
+}
+
+
+void traverse()
+{
+    struct node *trav;
+    trav = head;
+    if (head == NULL)
+        printf("\nList is empty\n\n");
+    else
+    {
+        printf("\n");
+        while (trav != NULL)
+        {
+            printf("%d\t", trav->data);
+            trav = trav->next;
+        }
+        printf("\n\n");
+    }
+}
+
+void at_begin();
+void at_random();
+void at_last();
+void traverse();
+void delete_first();
+void delete_last();
+void delete_random();
+struct node *create_node();
+int main()
+{
+    int ch;
+
+    while (1)
+    {
+        printf("\n1 : Insert at beginning\n");
+        printf("2 : Insert at last\n");
+        printf("3 : Insert at random\n");
+        printf("4 : Delete first\n");
+        printf("5 : Delete last\n");
+        printf("6 : Delete random\n");
+        printf("7 : Traverse\n");
+        printf("8 : Exit\n\n");
+
+        printf("\nEnter you choice : ");
+        scanf("%d", &ch);
+
+        switch (ch)
+        {
+        case 1:
+            at_begin();
+            break;
+
+        case 2:
+            at_last();
+            break;
+
+        case 3:
+            at_random();
+            break;
+
+        case 4:
+            delete_first();
+            break;
+
+        case 5:
+            delete_last();
+            break;
+
+        case 6:
+            delete_random();
+            break;
+
+        case 7:
+            traverse();
+            break;
+
+        case 8:
+            exit(0);
+
+        default:
+            printf("\nInvalid Choice\n");
+            break;
+        }
+    }
+    return 0;
+}
